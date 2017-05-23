c89993d4511733a55e803e9a5389dc4ab8445282
Saurav Sahu <sauravsahu.12@live.com>
2017-05-23 14:31:28 +0530

    Remove redundant stty calls

--- PartA/6/6b.sh
+++ PartA/6/6b.sh
@@ -2,14 +2,12 @@ echo Enter Password
 stty -echo
 read psd1
 
-stty echo
 echo Re-enter the Password
-stty -echo
 read psd2
-stty echo
 
 if [ "$psd1" = "$psd2" ]; then
     echo Passwords match
+    stty echo
     exit
 else 
     trap '' 2 
@@ -17,9 +15,7 @@ else
     while [ "$psd1" != "$psd2" ]
     do
         echo Wrong, re-enter again
-        stty -echo
         read psd2
-        stty echo
     done
 fi
 stty echo
